2f71193ff575fb74b5a37447404fd445f65d1cc5
Jim Geersinga <j.geersinga@outlook.com>
2015-11-09 11:36:06 +0100

    updated startup with the code from the old project

--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Startup.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Startup.cs
@@ -1,29 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Lisa.Excelsis.WebApi
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
+                options.SerializerSettings.MissingMemberHandling = MissingMemberHandling.Error;
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                   "CorsExcelsis",
+                    builder =>
+                    {
+                        builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+                    });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseCors("CorsExcelsis");
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
