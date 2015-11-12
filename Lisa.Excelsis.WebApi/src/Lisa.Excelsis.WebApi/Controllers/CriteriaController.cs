2a859b600e3454c9004fba2cfee38d76dfb0588d
Jim Geersinga <j.geersinga@outlook.com>
2015-11-12 13:09:57 +0100

    added criteria controller to add criteriums to an exam

---
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Controllers/CriteriaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Lisa.Excelsis.WebApi
+{
+    [Route("[controller]")]
+    public class CriteriaController : Controller
+    {
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] CriteriumPost criterium, int id)
+        {
+            if (id == 0 || !ModelState.IsValid || _db.AnyCriterium(id, criterium))
+            {
+                return new BadRequestResult();
+            }
+
+            _db.AddCriterium(id, criterium);
+            var result = _db.FetchExam(id);
+
+            return new CreatedResult("", result);
+        }
+
+        private readonly Database _db = new Database();
+    }
+}
