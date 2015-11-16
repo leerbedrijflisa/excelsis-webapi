1c93dcc3a13d637798e6f4a962683ee129527887
Arthur <arthurvanstrien@gmail.com>
2015-11-16 10:09:05 +0100

    Added SQL query to get subjects.

--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Subjects.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Subjects.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Collections.Generic;
+
+namespace Lisa.Excelsis.WebApi
+{
+    partial class Database
+    {
+        public IEnumerable<object> FetchSubjects()
+        {
+            var query = @"SELECT subject FROM Exams GROUP BY subject";
+            return _gateway.SelectMany(query);
+        }
+    }
+}
