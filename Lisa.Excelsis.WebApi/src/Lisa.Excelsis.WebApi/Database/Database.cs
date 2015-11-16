10de97c1c72c1b6776f30e8dcb709386d04651bb
Jim Geersinga <j.geersinga@outlook.com>
2015-11-16 12:48:35 +0100

    added error message list

--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Database.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Database.cs
@@ -1,15 +1,18 @@
 ﻿using Lisa.Common.Sql;
 using System;
+using System.Collections.Generic;
 
 namespace Lisa.Excelsis.WebApi
 {
     partial class Database : IDisposable
     {
+        public IEnumerable<string> ErrorMessage { get; set; }
+
         public void Dispose()
         {
             _gateway?.Dispose();
         }
-
+        
         private Gateway _gateway = new Gateway(@"Data Source=(localdb)\v11.0;Initial Catalog=ExcelsisDb;Integrated Security=True");
     }
 }
