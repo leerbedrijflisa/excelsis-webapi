c609dcc03ddae826f68f60cffa1e38a1020c2820
Arthur <arthurvanstrien@gmail.com>
2015-11-16 10:08:36 +0100

    Updated exam to exams to match the new database names.

--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Exam.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Exam.cs
@@ -6,27 +6,27 @@ namespace Lisa.Excelsis.WebApi
     {
         public object FetchExam(int id)
         {
-            var query = @"SELECT * FROM Exam WHERE Exam.Id = @id";
+            var query = @"SELECT * FROM Exams WHERE Exams.Id = @id";
             var parameters = new { id = id };
             return _gateway.SelectSingle(query, parameters);
         }
 
         public IEnumerable<object> FetchExams()
         {
-            var query = @"SELECT * FROM Exam";
+            var query = @"SELECT * FROM Exams";
             return _gateway.SelectMany(query);
         }
 
         public object AddExam(ExamPost exam)
         {
-            var query = @"INSERT INTO Exam (Name, Cohort, Crebo, Subject)
+            var query = @"INSERT INTO Exams (Name, Cohort, Crebo, Subject)
                           VALUES (@Name, @Cohort, @Crebo, @subject);";
             var parameters = new { Name = exam.Name, Cohort = exam.Cohort, Crebo = exam.Crebo, Subject = exam.Subject };
             return _gateway.Insert(query, parameters);
         }           
         public bool AnyExam(ExamPost exam)
         {
-            var query = @"SELECT COUNT(*) as count FROM Exam 
+            var query = @"SELECT COUNT(*) as count FROM Exams
                           WHERE Name LIKE @Name 
                             AND Subject LIKE @Subject 
                             AND Cohort LIKE @Cohort 
