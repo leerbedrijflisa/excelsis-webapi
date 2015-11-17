2d58d46b78244dbf1d0fe18e2055c0223748cefd
Jim Geersinga <j.geersinga@outlook.com>
2015-11-17 18:47:11 +0100

    updated sql script

--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/testDataScript.sql
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/testDataScript.sql
@@ -4,37 +4,38 @@ GO
 USE ExcelsisDb;
 
 CREATE TABLE [dbo].[Exams] (
-    [Id]      INT            IDENTITY (1, 1) NOT NULL,
-    [Name]    NVARCHAR (MAX) NULL,
-    [Cohort]  NVARCHAR (MAX) NULL,
-    [Crebo]   NVARCHAR (MAX) NULL,
-    [Subject] NVARCHAR (MAX) NULL,
-    [Created] DATETIME       DEFAULT (getutcdate()) NULL,
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (MAX) NULL,
+    [Cohort]        NVARCHAR (MAX) NULL,
+    [Crebo]         NVARCHAR (MAX) NULL,
+    [Subject]       NVARCHAR (MAX) NULL,
+    [Created]       DATETIME       DEFAULT (getutcdate()) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
-CREATE TABLE [dbo].[Criteriums] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [Order]       INT            NULL,
-	[Title]	      NVARCHAR(MAX)  NULL,
-    [Description] NVARCHAR (MAX) NULL,
-    [Value]       NVARCHAR (MAX) NULL,
-    [ExamId]      INT            NULL,
-    [Created]     DATETIME       DEFAULT (getutcdate()) NULL,
+CREATE TABLE [dbo].[Criteria] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Order]         INT            NULL,
+	[Title]	        NVARCHAR(MAX)  NULL,
+    [Description]   NVARCHAR (MAX) NULL,
+    [Value]         NVARCHAR (MAX) NULL,
+    [ExamId]        INT            NULL,
+    [Created]       DATETIME       DEFAULT (getutcdate()) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
 CREATE TABLE [dbo].[Assessors] (
-    [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [UserName] NVARCHAR (MAX) NULL,
-    [Email]    NVARCHAR (MAX) NULL,
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]      NVARCHAR (MAX) NULL,
+    [Email]         NVARCHAR (MAX) NULL,
+    [Created]       DATETIME       DEFAULT (getutcdate()) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
 CREATE TABLE [dbo].[AssessmentsAssessors] (
-    [Id]            INT IDENTITY (1, 1) NOT NULL,
-    [Assessment_Id] INT NULL,
-    [Assessor_Id]   INT NULL,
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Assessment_Id] INT            NULL,
+    [Assessor_Id]   INT            NULL,
     CONSTRAINT [PK_AssessmentsAssessors] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
@@ -44,7 +45,17 @@ CREATE TABLE [dbo].[Assessments] (
     [StudentNumber] NVARCHAR (MAX) NULL,
     [Assessed]      DATETIME       NULL,
     [Exam_Id]       INT            NULL,
-    [Created]     DATETIME       DEFAULT (getutcdate()) NULL,
+    [Created]       DATETIME       DEFAULT (getutcdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Observations] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Criterion_Id]  INT            NULL,
+    [Result]        NVARCHAR (MAX) NULL,
+    [Marks]         DATETIME       NULL,
+    [Assessment_Id] INT            NULL,
+    [Created]       DATETIME       DEFAULT (getutcdate()) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
@@ -107,7 +118,7 @@ INSERT INTO Exams (Name, Cohort, Crebo, Subject)
     ('Realiseren van een applicatie','2012','246859','Applicatieontwikkeling'),
     ('Opleveren van een applicatie','2012','246859','Applicatieontwikkeling'),
     ('Hoofdrekenen','2012','','Rekenen'),
-    ('Getallen','2012','','Rekenen');
+    ('Getallen','2012','','Rekenen');    
 
     DECLARE @timesToLoop INT
     DECLARE @innerLoopCount INT
@@ -121,9 +132,9 @@ INSERT INTO Exams (Name, Cohort, Crebo, Subject)
         WHILE @innerLoopCount < 15
         BEGIN
             SET @innerLoopCount = @innerLoopCount + 1
-            INSERT INTO Criteriums ([Order], Title,[Description], Value, ExamId)
+            INSERT INTO Criteria ([Order], Title,[Description], Value, ExamId)
             VALUES (@innerLoopCount, 'De kandidaat moet voldoen aan:', 'Beschrijving van de vraag','Goed', @timesToLoop)
         END
-        SET @innerLoopCount = 0 
+        SET @innerLoopCount = 0
     END;
     GO
