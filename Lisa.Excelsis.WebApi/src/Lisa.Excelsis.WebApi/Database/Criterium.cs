cd105df36db437096ca57988cbcaeef26b5c1129
Jim Geersinga <j.geersinga@outlook.com>
2015-11-17 10:15:48 +0100

    Added title to criterium

--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Criterium.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Criterium.cs
@@ -4,9 +4,9 @@
     {
         public object AddCriterium(int id, CriteriumPost criterium)
         {
-            var query = @"INSERT INTO Criteriums ([Order], [Description], Value, ExamId)
-                          VALUES (@Order, @Description, @Value, @ExamId);";
-            var parameters = new { Order = criterium.Order, Description = criterium.Description, Value = criterium.Value, ExamId = id };
+            var query = @"INSERT INTO Criteriums ([Order], Title, [Description], Value, ExamId)
+                          VALUES (@Order, @Title ,@Description, @Value, @ExamId);";
+            var parameters = new { Order = criterium.Order, Title = criterium.Title, Description = criterium.Description, Value = criterium.Value, ExamId = id };
             return _gateway.Insert(query, parameters);
         }
         public bool AnyCriterium(int id, CriteriumPost criterium)
--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/TransferObjects/CriteriumPost.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/TransferObjects/CriteriumPost.cs
@@ -7,6 +7,8 @@ namespace Lisa.Excelsis.WebApi
         [Required]
         public int Order { get; set; }
         [Required]
+        public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
         [Required]
         public string Value { get; set; }
