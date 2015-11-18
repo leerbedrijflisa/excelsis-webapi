33074bed56b7548a94fa3d0cb35ff766d4becbb7
Jim Geersinga <j.geersinga@outlook.com>
2015-11-18 10:15:39 +0100

    switched to localdb

--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Database.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Database/Database.cs
@@ -21,7 +21,7 @@ namespace Lisa.Excelsis.WebApi
 
         private List<string> _errorMessages { get; set; }
 
-        //private Gateway _gateway = new Gateway(@"Data Source=(localdb)\v11.0;Initial Catalog=ExcelsisDb;Integrated Security=True");
-        private Gateway _gateway = new Gateway(@"Server=tcp:leerbedrijflisa.database.windows.net,1433;Database=excelsis-develop-db;User ID=leerbedrijflisa@leerbedrijflisa;Password=j1*K36SF$PF#pG#M0M34ldb%zCWFYAP!F*%lNlp%Fc1o5VvyBP^YLj^hO;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        private Gateway _gateway = new Gateway(@"Data Source=(localdb)\v11.0;Initial Catalog=ExcelsisDb;Integrated Security=True");
+        //private Gateway _gateway = new Gateway(@"Server=tcp:leerbedrijflisa.database.windows.net,1433;Database=excelsis-develop-db;User ID=leerbedrijflisa@leerbedrijflisa;Password=j1*K36SF$PF#pG#M0M34ldb%zCWFYAP!F*%lNlp%Fc1o5VvyBP^YLj^hO;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
     }
 }
