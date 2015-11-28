e2dfe297c97d5e0ce681f6065b940b5810029237
Jim Geersinga <j.geersinga@outlook.com>
2015-11-28 11:28:40 +0100

    updated version

---
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Controllers/VersionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Lisa.Excelsis.WebApi.Controllers
+{
+    [Route("[controller]")]
+    public class VersionController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new HttpOkObjectResult(new { Version = "1.0.0-Alpha-2" });
+        }
+    }
+}
