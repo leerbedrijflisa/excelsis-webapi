81b17d5d0b241f3f9b365a180255782ded91e868
Arthur <arthurvanstrien@gmail.com>
2015-12-07 11:08:28 +0100

    added valuescontroller with a get marks.

---
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Lisa.Excelsis.WebApi.Controllers
+{
+    [Route("[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet("marks")]
+        public string GetMarks()
+        {
+            return "marks";
+        }
+    }
+}
