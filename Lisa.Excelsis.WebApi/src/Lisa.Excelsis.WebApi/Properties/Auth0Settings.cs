4f1688e54e59d1cd70adffd1432ca6fbc13d4b7b
Jim Geersinga <j.geersinga@outlook.com>
2016-01-25 15:44:52 +0100

    Added auth0 authentication

---
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Properties/Auth0Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiSample.Properties
+{
+    public class Auth0Settings
+    {
+        public string Domain { get; set; }
+
+        public string ClientId { get; set; }
+    }
+}
+
--- Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Startup.cs
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Startup.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Authentication.JwtBearer;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.OptionsModel;
+using Microsoft.Extensions.PlatformAbstractions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApiSample.Properties;
 
 namespace Lisa.Excelsis.WebApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment app)
+        {
+            this.Configuration = new ConfigurationBuilder()
+                .SetBasePath(app.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddJsonOptions(options =>
@@ -27,11 +47,39 @@ namespace Lisa.Excelsis.WebApi
             });
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
         {
             app.UseIISPlatformHandler();
             app.UseStaticFiles();
             app.UseCors("CorsExcelsis");
+            loggerfactory.AddConsole();
+
+            var logger = loggerfactory.CreateLogger("Auth0");
+            var settings = app.ApplicationServices.GetService<IOptions<Auth0Settings>>();
+
+            app.UseJwtBearerAuthentication(options =>
+            {
+                options.Audience = settings.Value.ClientId;
+                options.Authority = $"https://{settings.Value.Domain}";
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        logger.LogError("Authentication failed.", context.Exception);
+                        return Task.FromResult(0);
+                    },
+                    OnValidatedToken = context =>
+                    {
+                        var claimsIdentity = context.AuthenticationTicket.Principal.Identity as ClaimsIdentity;
+                        claimsIdentity.AddClaim(new Claim("id_token",
+                            context.Request.Headers["Authorization"][0].Substring(context.AuthenticationTicket.AuthenticationScheme.Length + 1)));
+
+                        // OPTIONAL: you can read/modify the claims that are populated based on the JWT
+                        // claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, claimsIdentity.FindFirst("name").Value));
+                        return Task.FromResult(0);
+                    }
+                };
+            });
             app.UseMvcWithDefaultRoute();
         }
     }
