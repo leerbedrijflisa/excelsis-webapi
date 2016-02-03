e74abb169e115093224d395853c4e56763c1ba51
Jim Geersinga <j.geersinga@outlook.com>
2016-02-03 16:15:56 +0100

    added profile controller

---
+++ Lisa.Excelsis.WebApi/src/Lisa.Excelsis.WebApi/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+
+namespace Lisa.Excelsis.WebApi
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        [Authorize(ActiveAuthenticationSchemes = "Bearer", Roles = "teacher,student")]
+        [Route("[controller]")]
+        public object Get()
+        {
+            return new
+            {
+                IsTeacher = User.IsInRole("teacher"),
+                IsStudent = User.IsInRole("student"),
+                IsAuthenticated = User.Identity.IsAuthenticated,
+                Profile = Startup.Profile
+            };
+        }
+    }
+}
